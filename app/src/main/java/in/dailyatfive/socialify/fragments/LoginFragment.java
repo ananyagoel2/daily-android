21c33533e183e4e9d0f286dcee02cf9e9c3bed6c
akshitkrnagpal <nagpalkrakshit@gmail.com>
2016-12-25 03:05:36 +0530

    Used Arrays.asList() for FB Permissions Save Login data to UserModel and sharedPrefs

--- app/src/main/java/in/dailyatfive/socialify/fragments/LoginFragment.java
+++ app/src/main/java/in/dailyatfive/socialify/fragments/LoginFragment.java
@@ -1,27 +1,32 @@
 package in.dailyatfive.socialify.fragments;
 
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.Profile;
-import com.facebook.ProfileTracker;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
+import java.util.Arrays;
+
+import in.dailyatfive.socialify.MainActivity;
 import in.dailyatfive.socialify.R;
+import in.dailyatfive.socialify.models.UserModel;
 
 public class LoginFragment extends Fragment {
 
@@ -50,21 +55,49 @@ public class LoginFragment extends Fragment {
         loginButton = (LoginButton) view.findViewById(R.id.fb_login_button);
         loginButton.setFragment(this);
 
-        loginButton.setReadPermissions("user_birthday");
-        loginButton.setReadPermissions("email");
-        loginButton.setReadPermissions("user_likes");
-        loginButton.setReadPermissions("user_tagged_places");
-        loginButton.setReadPermissions("user_photos");
-        loginButton.setReadPermissions("user_work_history");
-        loginButton.setReadPermissions("user_education_history");
-        loginButton.setReadPermissions("user_photos");
-
+        loginButton.setReadPermissions(
+                Arrays.asList(
+                        "user_birthday",
+                        "email",
+                        "user_likes",
+                        "user_tagged_places",
+                        "user_photos",
+                        "user_work_history",
+                        "user_education_history"
+                        )
+        );
 
         // Callback registration
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
                 Toast.makeText(getActivity(), "Login Successfull" , Toast.LENGTH_LONG).show();
+
+                SharedPreferences sharedPreferences = getActivity().getSharedPreferences("PREF_FILE", Context.MODE_PRIVATE);
+
+                UserModel.clearUser(sharedPreferences);
+
+                Profile profile = Profile.getCurrentProfile();
+                UserModel userModel = new UserModel();
+
+                while(profile==null);
+
+                userModel.setFb_id(profile.getId());
+                userModel.setFb_token(AccessToken.getCurrentAccessToken().toString());
+
+                userModel.setFirst_name(profile.getFirstName());
+                userModel.setLast_name(profile.getLastName());
+
+                userModel.setEmail("");
+                userModel.setGender("");
+                userModel.setMobile("");
+                userModel.setAdmin(false);
+
+                UserModel.saveUser(sharedPreferences,userModel);
+
+                Intent intent = new Intent(getActivity().getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                getActivity().finish();
             }
 
             @Override
