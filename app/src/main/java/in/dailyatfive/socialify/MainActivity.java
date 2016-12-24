eb23bf5751a2b664f07a0a4ce6ae0d9ac0704be7
akshitkrnagpal <nagpalkrakshit@gmail.com>
2016-12-24 22:58:25 +0530

    Created SplashActivity and MainActivity

---
+++ app/src/main/java/in/dailyatfive/socialify/MainActivity.java
@@ -0,0 +1,12 @@
+package in.dailyatfive.socialify;
+
+import android.os.Bundle;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/in/dailyatfive/socialify/SplashActivity.java
@@ -0,0 +1,16 @@
+package in.dailyatfive.socialify;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
