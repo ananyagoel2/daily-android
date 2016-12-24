e4b0b904ee7d31da03bfbf34c84098ada59e8532
akshitkrnagpal <nagpalkrakshit@gmail.com>
2016-12-25 00:09:57 +0530

    Made FB Login Button shown only on last SplashSlide

--- app/src/main/java/in/dailyatfive/socialify/SplashActivity.java
+++ app/src/main/java/in/dailyatfive/socialify/SplashActivity.java
@@ -1,20 +1,23 @@
 package in.dailyatfive.socialify;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
 import android.view.View;
 
 import in.dailyatfive.socialify.adapters.SplashSlideshowAdapter;
 
-public class SplashActivity extends BaseActivity {
+public class SplashActivity extends BaseActivity implements ViewPager.OnPageChangeListener {
 
     private ViewPager splashPager;
+    private FragmentManager fragmentManager;
 
     private static int NUM_PAGES = 4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        fragmentManager = getSupportFragmentManager();
 
 //        Intent intent = new Intent(SplashActivity.this,MainActivity.class);
 //        startActivity(intent);
@@ -24,9 +27,13 @@ public class SplashActivity extends BaseActivity {
 
         splashPager = (ViewPager) findViewById(R.id.splash_pager);
         SplashSlideshowAdapter splashSlideshowAdapter = new SplashSlideshowAdapter(getSupportFragmentManager(),NUM_PAGES);
-
         splashPager.setAdapter(splashSlideshowAdapter);
         splashPager.setCurrentItem(0);
+
+        splashPager.addOnPageChangeListener(this);
+
+        findViewById(R.id.fb_login_button).setVisibility(View.GONE);
+
 
     }
 
@@ -37,5 +44,22 @@ public class SplashActivity extends BaseActivity {
         } else {
             splashPager.setCurrentItem(splashPager.getCurrentItem() - 1);
         }
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        if(position == NUM_PAGES-1) {
+            findViewById(R.id.fb_login_button).setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
     }
 }
