30484488a2c78bc474025faf76ef12fbc00e749a
akshitkrnagpal <nagpalkrakshit@gmail.com>
2016-12-25 18:52:19 +0530

    Created RegEmailFragment

---
+++ app/src/main/java/in/dailyatfive/socialify/fragments/RegEmailFragment.java
@@ -0,0 +1,43 @@
+package in.dailyatfive.socialify.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import in.dailyatfive.socialify.R;
+import in.dailyatfive.socialify.models.UserModel;
+
+public class RegEmailFragment extends BaseFragment {
+
+
+    public RegEmailFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_reg_email, container, false);
+
+        EditText email = (EditText) view.findViewById(R.id.email_editbox);
+        email.setText(getArguments().getString("email"));
+        return view;
+    }
+
+    public static RegEmailFragment newInstance(UserModel userModel) {
+        RegEmailFragment f = new RegEmailFragment();
+        Bundle b = new Bundle();
+        b.putString("email", userModel.getEmail());
+        f.setArguments(b);
+        return f;
+    }
+
+}
