452fe62985871fe3b323b022736518fa8082ca84
akshitkrnagpal <nagpalkrakshit@gmail.com>
2016-12-25 21:18:41 +0530

    Added NonSwipeableViewPager

---
+++ app/src/main/java/in/dailyatfive/socialify/custom/NonSwipableViewPager.java
@@ -0,0 +1,27 @@
+package in.dailyatfive.socialify.custom;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+public class NonSwipableViewPager extends ViewPager {
+
+    public NonSwipableViewPager(Context context) {
+        super(context);
+    }
+
+    public NonSwipableViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return false;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        return false;
+    }
+}
