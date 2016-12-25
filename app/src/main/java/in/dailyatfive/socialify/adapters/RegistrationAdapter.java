f6cda9ef70f3c119c3c1405c1ebc7ad745c34498
akshitkrnagpal <nagpalkrakshit@gmail.com>
2016-12-25 18:06:54 +0530

    Created adapter for Registration viewPager

---
+++ app/src/main/java/in/dailyatfive/socialify/adapters/RegistrationAdapter.java
@@ -0,0 +1,32 @@
+package in.dailyatfive.socialify.adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import in.dailyatfive.socialify.fragments.RegProfileFragment;
+import in.dailyatfive.socialify.models.UserModel;
+
+public class RegistrationAdapter extends FragmentStatePagerAdapter {
+
+    private UserModel userModel;
+
+    public RegistrationAdapter(FragmentManager fm, UserModel userModel) {
+        super(fm);
+        this.userModel = userModel;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position) {
+            case 0 : return RegProfileFragment.newInstance(userModel);
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 1;
+    }
+}
