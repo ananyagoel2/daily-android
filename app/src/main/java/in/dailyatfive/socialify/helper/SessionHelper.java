177ecc193f0d93f8362d96322fe69edb27e3b523
akshitkrnagpal <nagpalkrakshit@gmail.com>
2016-12-29 01:25:20 +0530

    Created SessionHelper class

---
+++ app/src/main/java/in/dailyatfive/socialify/helper/SessionHelper.java
@@ -0,0 +1,42 @@
+package in.dailyatfive.socialify.helper;
+
+import android.content.SharedPreferences;
+
+import com.google.gson.Gson;
+
+import in.dailyatfive.socialify.network.models.User;
+
+public class SessionHelper {
+
+    public static void saveUser(SharedPreferences sharedPreferences,User user) {
+        SharedPreferences.Editor prefsEditor = sharedPreferences.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(user);
+        prefsEditor.putString("CurrentUser", json);
+        prefsEditor.commit();
+    }
+
+    public static User getUser(SharedPreferences sharedPreferences) {
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString("CurrentUser", "");
+        return gson.fromJson(json, User.class);
+    }
+
+    public static void clearUser(SharedPreferences sharedPreferences) {
+        sharedPreferences.edit().clear().apply();
+    }
+
+    public static boolean isLoggedIn(SharedPreferences sharedPreferences) {
+        return sharedPreferences.contains("CurrentUser");
+    }
+
+    public static void setJwtToken(SharedPreferences sharedPreferences,String jwtToken){
+        SharedPreferences.Editor prefsEditor = sharedPreferences.edit();
+        prefsEditor.putString("JWT", jwtToken);
+    }
+
+    public static String getJwtToken(SharedPreferences sharedPreferences){
+        return sharedPreferences.getString("JWT","");
+    }
+
+}
