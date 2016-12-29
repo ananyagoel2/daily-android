be3e054bb76625ffca9177a0928ef884ba279c03
akshitkrnagpal <nagpalkrakshit@gmail.com>
2016-12-30 01:56:44 +0530

    Created API method getUser()

--- app/src/main/java/in/dailyatfive/socialify/network/API.java
+++ app/src/main/java/in/dailyatfive/socialify/network/API.java
@@ -3,10 +3,12 @@ package in.dailyatfive.socialify.network;
 import java.util.Map;
 
 import in.dailyatfive.socialify.network.models.Register;
+import in.dailyatfive.socialify.network.models.User;
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FieldMap;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
@@ -15,6 +17,10 @@ import retrofit2.http.Path;
 public interface API {
 
     public static String BASEURL = "http://35.154.27.204:3000";
+
+    @GET("/users/{user_id}")
+    Call<User> getUser(@Path("user_id") String user_id ,
+                       @Header("Authorization") String authorization);
 
     @FormUrlEncoded
     @POST("/register")
