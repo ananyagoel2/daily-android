68ae5b8468b3eb60ee04a32970045f5fd1325e37
akshitkrnagpal <nagpalkrakshit@gmail.com>
2016-12-30 03:01:23 +0530

    Changed AppTheme from DarkActionBar to NoActionBar Added NavigationDrawer and ActionBar to MainActivity

--- app/src/main/java/in/dailyatfive/socialify/MainActivity.java
+++ app/src/main/java/in/dailyatfive/socialify/MainActivity.java
@@ -1,12 +1,91 @@
 package in.dailyatfive.socialify;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.Profile;
+import com.squareup.picasso.Picasso;
+
+import in.dailyatfive.socialify.helper.SessionHelper;
+import in.dailyatfive.socialify.network.models.User;
+import in.dailyatfive.socialify.ui.RegisterActivity;
 
 public class MainActivity extends BaseActivity {
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if(SessionHelper.isLoggedIn(sharedPreferences)) {
+            User user = SessionHelper.getUser(sharedPreferences);
+            if( user.getEmail() == null || user.getEmail() == "" || !user.getMobileVerified() ) {
+                Intent intent = new Intent(MainActivity.this,RegisterActivity.class);
+                startActivity(intent);
+                //finish();
+            }
+        }
+
         setContentView(R.layout.activity_main);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView) findViewById(R.id.design_navigation_view);
+        View headerView = navigationView.inflateHeaderView(R.layout.drawer_header);
+        ImageView profile_picture = (ImageView) headerView.findViewById(R.id.profile_pic_drawer);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        String image_link = Profile.getCurrentProfile().getProfilePictureUri(100,100).toString();
+
+        if(image_link != null && !image_link.equals(""))
+            Picasso.with(MainActivity.this)
+                    .load(image_link)
+                    .placeholder(R.drawable.default_profile_pic)
+                    .into(profile_picture);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.action_bar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        switch (id) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+            case R.id.action_settings:
+                Toast.makeText(MainActivity.this,"Settings",Toast.LENGTH_SHORT).show();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
