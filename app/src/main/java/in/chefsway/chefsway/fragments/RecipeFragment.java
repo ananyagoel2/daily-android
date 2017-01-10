f3f254d96540a6c59cd8453236a52b898f966cb1
akshitkrnagpal <nagpalkrakshit@gmail.com>
2017-01-11 03:35:26 +0530

    Created RecipeFragment with BardcodeScanning

---
+++ app/src/main/java/in/chefsway/chefsway/fragments/RecipeFragment.java
@@ -0,0 +1,66 @@
+package in.chefsway.chefsway.fragments;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.zxing.ResultPoint;
+import com.journeyapps.barcodescanner.BarcodeCallback;
+import com.journeyapps.barcodescanner.BarcodeResult;
+import com.journeyapps.barcodescanner.DecoratedBarcodeView;
+
+import java.util.List;
+
+import in.chefsway.chefsway.R;
+
+public class RecipeFragment extends BaseFragment {
+
+    private DecoratedBarcodeView barcodeScannerView;
+
+    public RecipeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    private BarcodeCallback callback = new BarcodeCallback() {
+        @Override
+        public void barcodeResult(BarcodeResult result) {
+            if(result.getText() == null ) {
+                return;
+            }
+            Toast.makeText(getActivity(),result.getText(),Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void possibleResultPoints(List<ResultPoint> resultPoints) {
+        }
+    };
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_recipe, container, false);
+        barcodeScannerView = (DecoratedBarcodeView) view.findViewById(R.id.zxing_barcode_scanner);
+        barcodeScannerView.decodeSingle(callback);
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        barcodeScannerView.resume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        barcodeScannerView.pause();
+    }
+
+}
