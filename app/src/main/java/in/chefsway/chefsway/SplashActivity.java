a5e15d1eb4a6b093ea61e2a45a5da52e9266d5a3
akshitkrnagpal <nagpalkrakshit@gmail.com>
2017-01-14 03:58:48 +0530

    Called getUser() to refresh data if logged in

--- app/src/main/java/in/chefsway/chefsway/SplashActivity.java
+++ app/src/main/java/in/chefsway/chefsway/SplashActivity.java
@@ -6,10 +6,18 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import in.chefsway.chefsway.adapters.SplashSlideshowAdapter;
 import in.chefsway.chefsway.helper.SessionHelper;
+import in.chefsway.chefsway.network.API;
+import in.chefsway.chefsway.network.models.User;
 import in.chefsway.chefsway.ui.LoginActivity;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class SplashActivity extends BaseActivity {
 
@@ -23,7 +31,41 @@ public class SplashActivity extends BaseActivity {
 
         if(SessionHelper.isLoggedIn(sharedPreferences)) {
 
-            // Login and refresh token
+            User user = SessionHelper.getUser(sharedPreferences);
+            String token = "JWT "+ SessionHelper.getJwtToken(sharedPreferences);
+
+            setContentView(R.layout.activity_splash_static);
+
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl(API.BASEURL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+
+            API api = retrofit.create(API.class);
+            retrofit2.Call<User> getUserCall = api.getUser(user.getId(),token);
+
+            getUserCall.enqueue(new Callback<User>() {
+                @Override
+                public void onResponse(Call<User> call, Response<User> response) {
+                    int code = response.code();
+                    if(code == 200) {
+
+                        SessionHelper.saveUser(sharedPreferences,response.body());
+
+                        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+
+                    } else if( code == 401 ) {
+                        Toast.makeText(SplashActivity.this,"Session Expired. Login again.",Toast.LENGTH_LONG).show();
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<User> call, Throwable t) {
+                    Toast.makeText(SplashActivity.this,"Error : Something went wrong..",Toast.LENGTH_LONG).show();
+                }
+            });
 
         } else {
 
