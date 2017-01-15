d3fd2898a1b269efc243e49bc064278c3b124584
akshitkrnagpal <nagpalkrakshit@gmail.com>
2017-01-16 02:54:14 +0530

    Modified RegistrationAdapter to use RegistrationFields list

--- app/src/main/java/in/chefsway/chefsway/adapters/RegistrationAdapter.java
+++ app/src/main/java/in/chefsway/chefsway/adapters/RegistrationAdapter.java
@@ -4,30 +4,27 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 
-import in.chefsway.chefsway.fragments.RegEmailFragment;
-import in.chefsway.chefsway.fragments.RegMobileFragment;
-import in.chefsway.chefsway.fragments.RegProfileFragment;
+import java.util.Arrays;
+import java.util.List;
+
 import in.chefsway.chefsway.fragments.RegistrationFragment;
 
 public class RegistrationAdapter extends FragmentStatePagerAdapter {
 
+    private List<String> RegistrationFields;
+
     public RegistrationAdapter(FragmentManager fm) {
         super(fm);
+        RegistrationFields = Arrays.asList("email","name","mobile");
     }
 
     @Override
     public Fragment getItem(int position) {
-
-        switch (position) {
-            case 0 : return RegistrationFragment.newInstance("name");
-            case 1 : return RegistrationFragment.newInstance("email");
-            case 2 : return RegistrationFragment.newInstance("mobile");
-        }
-        return null;
+        return RegistrationFragment.newInstance(RegistrationFields.get(position));
     }
 
     @Override
     public int getCount() {
-        return 3;
+        return RegistrationFields.size();
     }
 }
